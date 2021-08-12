35dc464b0e561e061b6305e3e8781166ec15ccb5
christianmsc <christian1601y@hotmail.com>
2021-08-11 23:28:27 -0300

    Menu and bottom navigation included

--- app/src/main/java/com/github/christianmsc/ui/MainActivity.kt
+++ app/src/main/java/com/github/christianmsc/ui/MainActivity.kt
@@ -2,11 +2,37 @@ package com.github.christianmsc.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.github.christianmsc.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.workoutFragment,
+                R.id.favoritesExercisesFragment,
+                R.id.randomExerciseFragment
+            )
+        )
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
